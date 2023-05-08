83fe1ddc0141fc8da40120b5b40b58053181bd90
zahid.khan <dead-dance@hotmail.com>
2023-05-08 15:34:19 +0600

    Add project files.

---
+++ WeatherCheckAPI/Controllers/CheckValueController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using WeatherCheckAPI.DTO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WeatherCheckAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CheckValueController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task GetFromAPI()
+        {
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://api.open-meteo.com/v1/forecast?latitude=23.71&longitude=90.41&hourly=temperature_2m&timeformat=unixtime");
+            request.UserAgent = "Developer";
+            request.Accept = "true";
+
+            try
+            {
+                System.Net.HttpWebResponse response = (System.Net.HttpWebResponse)request.GetResponse();
+                System.IO.Stream dataStream = response.GetResponseStream();
+                System.IO.StreamReader reader = new System.IO.StreamReader(dataStream);
+                var responseFromServer = reader.ReadToEnd();
+
+                Root? lc = JsonConvert.DeserializeObject<Root>(responseFromServer);
+
+                int i = 0;
+                for (i = 0; i < lc.hourly.time.Count; i++)
+                {
+                    DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(lc.hourly.time[i]));
+                    double temp = lc.hourly.temperature_2m[i];
+
+                    if(dateTimeOffset.DateTime.TimeOfDay.ToString() == "14:00:00")
+                    {
+                        Console.WriteLine(dateTimeOffset.DateTime + "," + temp);
+                    }
+
+                    //Console.WriteLine(dateTimeOffset.DateTime + "," + temp);
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response != null)
+                {
+                    var response = ex.Response;
+                    var dataStream = response.GetResponseStream();
+                    var reader = new StreamReader(dataStream);
+                    var details = reader.ReadToEnd();
+                }
+            }
+            catch (Exception tex)
+            {
+                if (tex.Message != null)
+                {
+                    var response = tex.Message;
+                }
+            }
+        }
+    }
+}
---
+++ WeatherCheckAPI/DTO/Root.cs
@@ -0,0 +1,39 @@
+﻿namespace WeatherCheckAPI.DTO
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class CurrentWeather
+    {
+        public string time { get; set; }
+        public double temperature { get; set; }
+        public int weathercode { get; set; }
+        public double windspeed { get; set; }
+        public int winddirection { get; set; }
+    }
+
+    public class Hourly
+    {
+        public List<string> time { get; set; }
+        public List<double> temperature_2m { get; set; }
+    }
+
+    public class HourlyUnits
+    {
+        public string temperature_2m { get; set; }
+    }
+
+    public class Root
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public double elevation { get; set; }
+        public double generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezone_abbreviation { get; set; }
+        public Hourly hourly { get; set; }
+        public HourlyUnits hourly_units { get; set; }
+        public CurrentWeather current_weather { get; set; }
+    }
+
+
+}
